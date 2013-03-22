3bdb348b9ddaa37db628861e93fc9f517631786c
gseva <sebastiang@US64-Dev.(none)>
2013-03-22 13:25:11 -0300

    Cambios minusiosos y agregado de comentarios

--- src/patentes.py
+++ src/patentes.py
@@ -9,6 +9,7 @@ import re
 
 
 def main():
+    """La funcion madre, interroga al usuario"""
     patente = ""
     promt = "Ingrese su patente o 'q' para salir: "
     while patente != "q":
@@ -18,6 +19,9 @@ def main():
 
 
 def crear_diccionario():
+    """Crea un diccionario asigandole un valor de la tabla
+       a todos los caracteres low case ASCII. Hice esta funcion
+       porque me daba fiaca escribir el diccionario a mano"""
     lista = "14 01 00 16 05 02 19 09 24 07 21 08 04\
              13 25 22 18 10 02 06 12 23 11 03 15 17"
     diccionario = {}
@@ -29,6 +33,8 @@ def crear_diccionario():
 
 
 def verificacion_longitud(valor):
+    """Verifica que cada valor sea menor que 10, en caso
+       contrario vuelvo a sumar sus digitos"""
     nuevo_valor = valor
     while nuevo_valor >= 10:
         nuevo_valor = 0
@@ -40,6 +46,8 @@ def verificacion_longitud(valor):
 
 
 def calculo_digito(lista):
+    """Devuelve la suma reversa alternada de los
+       valores de una lista pasada """
     valor_1 = 0
     valor_2 = 0
     iterador = len(lista)
@@ -51,12 +59,14 @@ def calculo_digito(lista):
         iterador -= 1
     valor_1 = verificacion_longitud(valor_1)
     valor_2 = verificacion_longitud(valor_2)
-    respuesta = "Su digito de verificacion es:",\
+    respuesta = "Su digito de verificacion es: " + \
                 "".join([str(valor_2), str(valor_1)])
     return respuesta
 
 
 def reemplazo_patente(patente=""):
+    """Verifica que la patente sea letras seguidas de numeros,
+       y la convierte en una lista de enteros de 1 digito"""
     lista = []
     pattern = re.compile("([a-zA-Z]+)([0-9]+)")
     clave_separada = re.match(pattern, patente)

e8d25a1d2d2ea20b13e4de23c60077571676ba63
gseva <sebastiang@US64-Dev.(none)>
2013-04-08 10:46:19 -0300

    mejorado el algoritmo aun mas, agregada una simple implemetacion de prioridad. Ahora para algunos casos llega a las 64 movidas

--- src/caballo.py
+++ src/caballo.py
@@ -5,39 +5,50 @@ Created on 05/04/2013
 '''
 
 llamadas = 0
+from Queue import PriorityQueue
 
 
 class cobayo_handler():
 
-    def __init__(self, pos):
-        self.pos = pos
+    def __init__(self):
         self.movimientos_prohibidos = {}
-#        self.lista_movs = []
         self.pos_actual = ()
         self.pos_futura = ()
-#        self.posicion_inicial = ()
-        self.lista_listas = {}
         self.lista_max = []
         self.movimientos = [(2, 1), (2, -1), (1, 2), (-1, 2),
                             (-2, 1), (-2, -1), (-1, -2), (1, -2)]
         self.llamadas = 0
 
-    def calcular_posicion(self, pos, index, i, m_p):
-        if index > 8:
+    def calcular_posicion(self, pos, i, m_p):
+        posibles_movimientos = []
+        cola = PriorityQueue()
+        for index in range(8):
+            x = pos[0] + self.movimientos[index][0]
+            y = pos[1] + self.movimientos[index][1]
+            if not (x > 8 or x < 1 or y > 8 or y < 1):
+                if m_p[i].__contains__((x, y)):
+                    continue
+                posibles_movimientos.append((x, y))
+        if not posibles_movimientos:
             return None
-        x = pos[0] + self.movimientos[index][0]
-        y = pos[1] + self.movimientos[index][1]
-        if not (x > 8 or x < 1 or y > 8 or y < 1):
-            if m_p[i].__contains__((x, y)):
-                return None
-#            try:
-            if self.lista_listas[i].__contains__((x, y)):
-                return None
-#            except:
-#                pass
-            tupla = (x, y)
-            return tupla
-        return None
+        for item in posibles_movimientos:
+            prioridad = 100
+            x = item[0]
+            y = item[1]
+            if x == 1 or x == 8:
+                prioridad -= 10
+            if y == 1 or y == 8:
+                prioridad -= 10
+            if x == 2 or x == 7:
+                prioridad -= 5
+            if y == 2 or y == 7:
+                prioridad -= 5
+            if x == 3 or x == 5:
+                prioridad -= 1
+            if y == 3 or y == 5:
+                prioridad -= 1
+            cola.put((prioridad, item))
+        return cola.get()[1]
 
     def hacer_movimiento(self, pos_inicial):
 
@@ -46,7 +57,6 @@ class cobayo_handler():
         self.lista_movs = []
         self.lista_movs.append(pos_inicial)
         self.llamadas += 1
-#        self.movimientos_prohibidos = {}
         i = 0
 
         while True:
@@ -54,35 +64,22 @@ class cobayo_handler():
                 self.lista_max = self.lista_movs[:]
             if not i in self.movimientos_prohibidos.keys():
                 self.movimientos_prohibidos[i] = []
-            if not i in self.lista_listas.keys():
-                self.lista_listas[i] = []
             for item in self.lista_movs:
                 if not item in self.movimientos_prohibidos[i]:
                     self.movimientos_prohibidos[i].append(item)
-            for iterador in range(8):
-                self.pos_futura = self.calcular_posicion(self.pos_actual,
-                                  iterador, i, self.movimientos_prohibidos)
-                if self.pos_futura:
-                    break
+            self.pos_futura = self.calcular_posicion(self.pos_actual,
+                                      i, self.movimientos_prohibidos)
             if not self.pos_futura:
-#                if contador < 1:
-#                    return self.lista_max
-#                contador -= 1
-#                pos_anterior = self.lista_movs.pop()
-#                self.movimientos_prohibidos[i].append(self.pos_actual)
-#                self.pos_actual = pos_anterior
-                self.lista_listas[i].append(self.pos_actual)
                 self.movimientos_prohibidos[i - 1].append(self.pos_actual)
-#                print " llegue a hacer", len(self.lista_movs), "movimientos"
-                try:
+                if self.llamadas <= 5:
                     return self.hacer_movimiento(pos_inicial)
-                except:
-                    return self.lista_max
+                else:
+                    break
             else:
                 self.lista_movs.append(self.pos_futura)
+                i += 1
                 if len(self.lista_movs) >= 64:
                     return self.lista_movs
-                i += 1
                 self.pos_actual = self.pos_futura
                 continue
         return self.lista_max
@@ -91,9 +88,8 @@ lista_cant_mov = []
 for x in range(8):
     for y in range(8):
         pos = (x + 1, y + 1)
-        ch = cobayo_handler(pos)
+        ch = cobayo_handler()
         lista_movs = ch.hacer_movimiento(pos)
-
         lista_tablero = {}
         counter = 1
         for item in lista_movs:
@@ -114,6 +110,12 @@ for x in range(8):
                 string += " . "
             if (i + 1) % 8 == 0:
                 string += "\n"
-
         print string
+
 print lista_cant_mov
+#l1 = [60, 64, 60, 61, 62, 63, 62, 62, 61, 63, 64, 62, 64, 63, 61, 60, 61, 64,
+#      60, 62, 61, 63, 63, 62, 62, 62, 62, 60, 63, 63, 62, 60, 59, 62, 62, 61,
+#      61, 60, 62, 62, 61, 64, 64, 63, 64, 60, 64, 62, 63, 63, 62, 62, 60, 61,
+#      64, 64, 62, 60, 62, 62, 61, 60, 62, 63]
+#print l1
+#print l1 == lista_cant_mov

da22211f3a03a10d31c7c380fbb82d397e9e53e7
gseva <sebastiang@US64-Dev.(none)>
2013-04-08 09:23:13 -0300

    mejorado el algoritmo, ahora saca mas posibilidades pero aun no llega al maximo

--- src/caballo.py
+++ src/caballo.py
@@ -7,80 +7,101 @@ Created on 05/04/2013
 llamadas = 0
 
 
-def calcular_posicion(pos, index, i, m_p):
-    if index > 8:
-        return None
-    movimientos = [(2, 1), (2, -1), (1, 2), (-1, 2),
-                   (-2, 1), (-2, -1), (-1, -2), (1, -2)]
-    x = pos[0] + movimientos[index][0]
-    y = pos[1] + movimientos[index][1]
-    if not (x > 8 or x < 1 or y > 8 or y < 1):
-        if m_p[i].__contains__((x, y)):
+class cobayo_handler():
+
+    def __init__(self, pos):
+        self.pos = pos
+        self.movimientos_prohibidos = {}
+#        self.lista_movs = []
+        self.pos_actual = ()
+        self.pos_futura = ()
+#        self.posicion_inicial = ()
+        self.lista_listas = {}
+        self.lista_max = []
+        self.movimientos = [(2, 1), (2, -1), (1, 2), (-1, 2),
+                            (-2, 1), (-2, -1), (-1, -2), (1, -2)]
+        self.llamadas = 0
+
+    def calcular_posicion(self, pos, index, i, m_p):
+        if index > 8:
             return None
-        tupla = (x, y)
-        return tupla
-    return None
+        x = pos[0] + self.movimientos[index][0]
+        y = pos[1] + self.movimientos[index][1]
+        if not (x > 8 or x < 1 or y > 8 or y < 1):
+            if m_p[i].__contains__((x, y)):
+                return None
+#            try:
+            if self.lista_listas[i].__contains__((x, y)):
+                return None
+#            except:
+#                pass
+            tupla = (x, y)
+            return tupla
+        return None
 
+    def hacer_movimiento(self, pos_inicial):
 
-def hacer_movimiento(pos_actual):
-    movimientos_prohibidos = {}
-    i = 0
-    global llamadas
-    lista = []
-    lista_max = []
-    lista.append(pos_actual)
-    llamadas += 1
+        self.pos_inicial = ()
+        self.pos_actual = pos_inicial
+        self.lista_movs = []
+        self.lista_movs.append(pos_inicial)
+        self.llamadas += 1
+#        self.movimientos_prohibidos = {}
+        i = 0
 
-    while True:
-        if len(lista) > len(lista_max):
-            lista_max = lista[:]
-        if not i in movimientos_prohibidos.keys():
-            movimientos_prohibidos[i] = []
-        try:
-            movimientos_prohibidos[i].append(lista[-1])
-            for item in lista:
-                if not item in movimientos_prohibidos[i]:
-                    movimientos_prohibidos[i].append(item)
-        except:
-            pass
-        for iterador in range(8):
-            pos_futura = calcular_posicion(pos_actual, iterador, i,
-                                           movimientos_prohibidos)
-            if pos_futura:
-                break
-        if not pos_futura:
-            if i < 1:
-                return lista_max
-            i -= 1
-            pos_anterior = lista.pop()
-            movimientos_prohibidos[i].append(pos_actual)
-            pos_actual = pos_anterior
-            continue
-        else:
-            lista.append(pos_futura)
-            if len(lista) >= 64:
-                return lista
-            i += 1
-            pos_actual = pos_futura
-            continue
-    return lista_max
-
+        while True:
+            if len(self.lista_movs) > len(self.lista_max):
+                self.lista_max = self.lista_movs[:]
+            if not i in self.movimientos_prohibidos.keys():
+                self.movimientos_prohibidos[i] = []
+            if not i in self.lista_listas.keys():
+                self.lista_listas[i] = []
+            for item in self.lista_movs:
+                if not item in self.movimientos_prohibidos[i]:
+                    self.movimientos_prohibidos[i].append(item)
+            for iterador in range(8):
+                self.pos_futura = self.calcular_posicion(self.pos_actual,
+                                  iterador, i, self.movimientos_prohibidos)
+                if self.pos_futura:
+                    break
+            if not self.pos_futura:
+#                if contador < 1:
+#                    return self.lista_max
+#                contador -= 1
+#                pos_anterior = self.lista_movs.pop()
+#                self.movimientos_prohibidos[i].append(self.pos_actual)
+#                self.pos_actual = pos_anterior
+                self.lista_listas[i].append(self.pos_actual)
+                self.movimientos_prohibidos[i - 1].append(self.pos_actual)
+#                print " llegue a hacer", len(self.lista_movs), "movimientos"
+                try:
+                    return self.hacer_movimiento(pos_inicial)
+                except:
+                    return self.lista_max
+            else:
+                self.lista_movs.append(self.pos_futura)
+                if len(self.lista_movs) >= 64:
+                    return self.lista_movs
+                i += 1
+                self.pos_actual = self.pos_futura
+                continue
+        return self.lista_max
 
 lista_cant_mov = []
 for x in range(8):
     for y in range(8):
         pos = (x + 1, y + 1)
-        lista = hacer_movimiento(pos)
+        ch = cobayo_handler(pos)
+        lista_movs = ch.hacer_movimiento(pos)
 
         lista_tablero = {}
         counter = 1
-        for item in lista:
+        for item in lista_movs:
             lista_tablero[(item[0] - 1) * 8 + (item[1] - 1)] = counter
             counter += 1
-
         print "empezando en la pos: ", pos
-        print "maxima cantidad de moviemientos:", len(lista)
-        lista_cant_mov.append(len(lista))
+        print "maxima cantidad de moviemientos:", len(lista_movs)
+        lista_cant_mov.append(len(lista_movs))
         string = ""
         for i in range(64):
             if i in lista_tablero.keys():
